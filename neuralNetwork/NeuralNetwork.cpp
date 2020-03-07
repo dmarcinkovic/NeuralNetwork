8ee95f875fa69ec27ce1315439cca0911ca9090c
david <david.marcinkovic@fer.hr>
2020-03-07 21:28:24 +0100

    Created neural network class

---
+++ neuralNetwork/NeuralNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 07. 03. 2020..
+//
+
+#include "NeuralNetwork.h"
---
+++ neuralNetwork/NeuralNetwork.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 07. 03. 2020..
+//
+
+#ifndef NEURALNETWORK_NEURALNETWORK_H
+#define NEURALNETWORK_NEURALNETWORK_H
+
+
+class NeuralNetwork
+{
+    
+};
+
+
+#endif //NEURALNETWORK_NEURALNETWORK_H
