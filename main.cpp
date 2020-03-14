d417af9eb19078c30675782126f087aa845314ef
david <david.marcinkovic@fer.hr>
2020-03-14 15:36:47 +0100

    Defined network size

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 int main()
 {
+    constexpr const int INPUT = 28 * 28;
+    constexpr const int HIDDEN = 64;
+    constexpr const int OUTPUT = 10;
+    
     std::vector<Data> labeledData = Data::loadLabeledData("trainingData");
     Data::shuffleData(labeledData);
 
