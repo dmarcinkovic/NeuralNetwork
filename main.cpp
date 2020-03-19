828c28a97e906b6fd01bb3e1361155e62040f4d9
david <david.marcinkovic@fer.hr>
2020-03-19 22:28:34 +0100

    Refactored code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 
 #include "imageLoader/Data.h"
 #include "neuralNetwork/NeuralNetwork.h"
-#include <chrono>
+#include "train/Train.h"
 
 int main()
 {
@@ -9,45 +9,11 @@ int main()
     constexpr const int HIDDEN = 256;
     constexpr const int OUTPUT = 10;
 
-//    std::vector<Data> input = Data::loadLabeledData("trainingData");
-    std::vector<Data> testData = Data::loadLabeledData("testData");
-
     NeuralNetwork<INPUT, HIDDEN, OUTPUT> network;
 
-    network.loadTrainedModel("trainedModel.txt");
+    Train::train("trainingData", network);
 
-//    std::cout << "Done preparing data\n";
-//
-//    double totalDuration = 0;
-//    for (int j = 0; j < 1; ++j)
-//    {
-//        auto start = std::chrono::high_resolution_clock::now();
-//        for (auto &i : input)
-//        {
-//            network.train(i.getData(), Data::getAnswerData(i.getLabel()));
-//        }
-//        auto end = std::chrono::high_resolution_clock::now();
-//        double duration =  std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-//        std::cout << "Time took: " << duration << " s\n";
-//
-//        totalDuration += duration;
-//    }
-//    std::cout << "Total time: " << totalDuration << "s\n";
-
-    int correct = 0;
-    for (auto &i : testData)
-    {
-        int guess = network.guess(i.getData());
-
-        if (guess == std::stoi(i.getLabel()))
-        {
-            ++correct;
-        }
-    }
-
-    std::cout << "Correct: " << static_cast<double>(correct) / testData.size() * 100 << "%\n";
-
-    //network.saveTrainedModel("trainedModel.txt");
+    Train::test("testData", network);
 
     return 0;
 }
---
+++ train/Train.h
@@ -0,0 +1,62 @@
+//
+// Created by david on 19. 03. 2020..
+//
+
+#ifndef NEURALNETWORK_TRAIN_H
+#define NEURALNETWORK_TRAIN_H
+
+#include <vector>
+#include <chrono>
+
+#include "../imageLoader/Data.h"
+#include "../neuralNetwork/NeuralNetwork.h"
+
+class Train
+{
+public:
+    template<int INPUT, int HIDDEN, int OUTPUT>
+    static void train(const char *directoryName, NeuralNetwork<INPUT, HIDDEN, OUTPUT> &network)
+    {
+        std::vector<Data> trainData = Data::loadLabeledData(directoryName);
+
+        constexpr static const int numberOfEpochs = 1;
+
+        double totalDuration = 0;
+        for (int j = 0; j < numberOfEpochs; ++j)
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            for (auto &i : trainData)
+            {
+                network.train(i.getData(), Data::getAnswerData(i.getLabel()));
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            totalDuration += std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+
+            std::cout << "Completed: " << static_cast<double>(j + 1) / numberOfEpochs * 100 << "%\n";
+        }
+
+        std::cout << "Time took: " << totalDuration << " s\n";
+    }
+
+    template<int INPUT, int HIDDEN, int OUTPUT>
+    static void test(const char *directoryName, const NeuralNetwork<INPUT, HIDDEN, OUTPUT> &network)
+    {
+        std::vector<Data> testData = Data::loadLabeledData(directoryName);
+
+        int correct = 0;
+        for (auto &i : testData)
+        {
+            int guess = network.guess(i.getData());
+
+            if (guess == std::stoi(i.getLabel()))
+            {
+                ++correct;
+            }
+        }
+
+        std::cout << "Correct: " << static_cast<double>(correct) / testData.size() * 100 << "%\n";
+    }
+};
+
+
+#endif //NEURALNETWORK_TRAIN_H
