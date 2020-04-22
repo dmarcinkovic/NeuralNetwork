988e3bb4a27499069f898505a47448eb3adb37d8
david <david.marcinkovic@fer.hr>
2020-04-22 21:38:41 +0200

    Added test sample

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
     std::vector<std::string> labels{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
     Train train(labels, IMAGE_WIDTH, IMAGE_HEIGHT);
-    train.predict("/home/david/sketchbook/Paint/image.png", network);
+    train.predict("testSample/", network);
 
     return 0;
 }
