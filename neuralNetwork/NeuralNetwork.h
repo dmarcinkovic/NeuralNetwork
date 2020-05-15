9b5c2d6c0b3f2c2c6f8e7be97bd8db124adff8c0
david <david.marcinkovic@fer.hr>
2020-05-15 19:14:55 +0200

    Wrote documentation for neural network class

--- neuralNetwork/NeuralNetwork.h
+++ neuralNetwork/NeuralNetwork.h
@@ -11,6 +11,33 @@
 #include <iostream>
 #include <fstream>
 
+
+/**
+ *
+ *  Class represents simple neural network which consists of only one
+ *  hidden layer. <br>
+ *  Example of use: <pre>
+ *      // Labels are: dog, cat <br>
+ *      constexpr int INPUT = 2; <br>
+ *      constexpr int HIDDEN = 2; <br>
+ *      constexpr int  OUTPUT = 2; <br> <br>
+ *      NeuralNetwork&lt INPUT, HIDDEN, OUTPUT > network; <br> <br>
+ *      // prepareData loads input data. For example loads image pixels. <br>
+ *      Vector&lt INPUT > input = prepareData(); <br>
+ *      Vector&lt OUTPUT > output{0, 1}; // Expected value is cat <br> <br>
+ *      network.train(input, output); <br>
+ *      Vector&lt INPUT > test = prepareData(); <br> <br>
+ *      // Returns index 0 if prediction is dog. If prediction is cat returns 1. <br>
+ *      int guess = network.guess(test); <br>
+ *      network.saveTrainedModel("file.txt"); // Save trained model
+ *  </pre>
+ *
+ * @author David
+ *
+ * @tparam INPUT Number of nodes in input layer.
+ * @tparam HIDDEN Number of nodes in hidden layer.
+ * @tparam OUTPUT Number of nodes in output layer.
+ */
 template<int INPUT, int HIDDEN, int OUTPUT>
 class NeuralNetwork
 {
@@ -37,12 +64,31 @@ public:
         m_HiddenBias = Vector<OUTPUT>::getRandomVector(-1, 1);
     }
 
+    /**
+     * Method used to train this neural network.
+     *
+     * @param inputNodes Input data represented as vector of size
+     * equal to number of nodes in input layer.
+     *
+     * @param answer Labeled data represented as vector of size
+     * equal to number of nodes in output layer.
+     */
     void train(const Vector<INPUT> &inputNodes, const Vector<OUTPUT> &answer)
     {
         feedForward(inputNodes);
         backPropagation(answer);
     }
 
+    /**
+     * Used to predict output for given input data.
+     *
+     * @param input Input data represented as vector of size equal
+     * to number of nodes in input layer.
+     *
+     * @return Integer which represents the index (starting from zero) of given label.
+     * For example, if labels are: dog, cat and mouse. If correct answer is cat, this
+     * method will return number 1.
+     */
     int guess(const Vector<INPUT> &input) const
     {
         Vector<HIDDEN> hiddenNodes = m_InputHiddenWeights * input + m_InputBias;
@@ -55,6 +101,12 @@ public:
         return maxNumber - output.begin();
     }
 
+    /**
+     * Saves the weights and biases of the the given neural network so
+     * that it is not necessary to re-train neural network during the next use.
+     *
+     * @param filename File in which all weights and biases will be stored.
+     */
     void saveTrainedModel(const char *filename) const
     {
         std::ofstream writer(filename);
@@ -65,6 +117,12 @@ public:
         writer.close();
     }
 
+    /**
+     * Loads the weights and biases of the given neural network so
+     * that it is not necessary to re-train neural network in every use.
+     *
+     * @param filename File in which all weights and biases are stored.
+     */
     void loadTrainedModel(const char *filename)
     {
         std::ifstream reader(filename);
@@ -77,6 +135,11 @@ public:
 
 private:
 
+    /**
+     * Implementation of feed forward algorithm.
+     * @param inputNodes Input data represented as vector of size equal
+     * to number of nodes in input layer.
+     */
     void feedForward(const Vector<INPUT> &inputNodes)
     {
         m_InputNodes = inputNodes;
@@ -88,6 +151,12 @@ private:
         m_OutputNodes.map(Util::getSigmoid());
     }
 
+    /**
+     * Implementation of backpropagation algorithm.
+     *
+     * @param answer Labeled data represented as vector of size
+     * equal to number of nodes in output layer.
+     */
     void backPropagation(const Vector<OUTPUT> &answer)
     {
         Vector<OUTPUT> outputError = calculateError(answer, m_OutputNodes);
@@ -105,6 +174,16 @@ private:
         m_InputBias += hiddenGradient;
     }
 
+    /**
+     * Method used to calculate error of output layer of neural network.
+     *
+     * @tparam N Number of nodes in given layer.
+     * @param answer Labeled data represented as vector of size
+     * equal to number of nodes in output layer.
+     * @param guess Output of neural network, i.e prediction.
+     *
+     * @return Vector representing error that occurred in the output layer of neural network.
+     */
     template<int N>
     static Vector<N> calculateError(const Vector<N> &answer, const Vector<N> &guess)
     {
